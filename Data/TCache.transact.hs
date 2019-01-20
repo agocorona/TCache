00e040f1c039667456f65dd6d97315583c521de5
Brandon Elam Barker <beb82@cornell.edu>
2019-01-20 15:18:47 +0000

    some doc cleanup

--- Data/TCache.transact.hs
+++ Data/TCache.transact.hs
@@ -2,23 +2,22 @@
     , FlexibleInstances, UndecidableInstances #-} 
 
 {- | TCache is a transactional cache with configurable persistence that permits
-STM transactions with objects thar syncronize sincromous or asyncronously with
+STM transactions with objects thar synchronize synchronously or asynchronously with
 their user defined storages. Default persistence in files is provided for testing purposes
 
- TCache implements ''DBRef' 's . They are persistent STM references  with a traditional 'readDBRef', 'writeDBRef' Haskell interface.
-simitar to TVars ('newDBRef', 'readDBRef', 'writeDBRef' etc) but with added. persistence
-. DBRefs are serializable, so they can be stored and retrieved. See some examples below
-Because they are references,they point to other serializable registers.
-This permits persistent mutable Inter-object relations
+ TCache implements 'DBRef's . They are persistent STM references with a traditional 'readDBRef', 'writeDBRef' Haskell interface
+similar to TVars ('newDBRef', 'readDBRef', 'writeDBRef' etc) but with added persistence. 
+DBRefs are serializable, so they can be stored and retrieved. See some examples below.
+Because they are references, they point to other serializable registers.
+This permits persistent mutable inter-object relations.
 
 For simple transactions of lists of objects of the same type TCache implements
-inversion of control primitives 'withSTMResources' and variants, that call pure user defined code for registers update.
+inversion of control primitives 'withSTMResources' and variants, that call pure user-defined code for registers update.
 See examples below.
 
 Triggers in "Data.TCache.Triggers" are user defined hooks that are called back on register updates. That can be used for:
 
-
-"Data.TCache.IndexQuery" implements an straighforwards pure haskell type safe query language  based
+"Data.TCache.IndexQuery" implements a straightforward pure Haskell, type-safe query language based
  on register field relations. This module must be imported separately.
 
 "Data.TCache.IndexText" add full text search and content search to the qhery language
@@ -40,33 +39,32 @@ module Data.TCache (
 -- * synced atomical transaction
  ,atomicallySync
 -- * Operations with cached database references
-{-|  DBRefs are persistent cached database references in the STM monad
+{-|  'DBRef's are persistent cached database references in the STM monad
 with read/write primitives, so the traditional syntax of Haskell STM references
-can be used for  interfacing with databases. As expected, the DBRefs are transactional,
+can be used for interfacing with databases. As expected, the DBRefs are transactional,
  because they operate in the STM monad.
 
-DBRefs are references to  cached database objects. A DBRef is associated with its referred object and its key
-Since DBRefs are serializable, they can be elements of mutable objects. They could point to other mutable objects
-and so on, so DBRefs can act as "hardwired" relations from mutable objects
-to other mutable objects in the database/cache. their referred objects are loaded, saved and flused
-to and from the cache automatically depending on the cache handling policies and the access needs
+A @DBRef@ is associated with its referred object trough its key.
+Since DBRefs are serializable, they can be elements of mutable cached objects themselves.
+They could point to other mutable objects
+and so on, so DBRefs can act as \"hardwired\" relations from mutable objects
+to other mutable objects in the database/cache. their referred objects are loaded, saved and flushed
+to and from the cache automatically depending on the cache handling policies and the access needs.
 
+@DBRefs@ are univocally identified by its object keys, so they can be compared, ordered, checked for equality, and so on.
+The creation of a DBRef, though 'getDBRef' is pure. This permits an efficient lazy access to the
+ registers through their DBRefs by lazy marshalling of the register content on demand.
 
-DBRefs are univocally identified by its pointed object keys, so they can be compared, ordered and so on.
-The creation of a DBRef, trough 'getDBRef' is pure. This permits an efficient lazy marshalling
-of registers with references, such are indexes when are queried for some fields but not others.
-
-Example: Car registers have references to Person regiters
+Example: Car registers have references to Person registers.
 
 @
 data Person= Person {pname :: String} deriving  (Show, Read, Eq, Typeable)
 data Car= Car{owner :: DBRef Person , cname:: String} deriving (Show, Read, Eq, Typeable)
 @
 
+Here the Car register point to the Person register through the owner field.
 
-Here the Car register point to the Person register trough the owner field
-
-To permit persistence and being refered with DBRefs, define the Indexable instance
+To permit persistence and being referred with DBRefs, define the 'Indexable' instance
 for these two register types:
 
 @
@@ -76,7 +74,7 @@ instance Indexable Car where key Car{cname= n} = "Car " ++ n
 
 Now we create a DBRef to a Person whose name is \"Bruce\"
 
->>> let bruce =   getDBRef . key $ Person "Bruce" :: DBRef Person
+>>> let bruce = getDBRef . key $ Person "Bruce" :: DBRef Person
 
 >>> show bruce
 >"DBRef \"Person bruce\""
@@ -86,9 +84,9 @@ Now we create a DBRef to a Person whose name is \"Bruce\"
 
 'getDBRef' is pure and creates the reference, but not the referred object;
 To create both the reference and the DBRef, use 'newDBRef'.
-Lets create two Car's and its two Car DBRefs with bruce as owner:
+Lets create two Cars and its two Car DBRefs with bruce as owner:
 
->>> cars <- atomically $  mapM newDBRef [Car bruce "Bat Mobile", Car bruce "Porsche"]
+>>> cars <- atomically $ mapM newDBRef [Car bruce "Bat Mobile", Car bruce "Porsche"]
 
 >>> print cars
 >[DBRef "Car Bat Mobile",DBRef "Car Porsche"]
@@ -97,30 +95,27 @@ Lets create two Car's and its two Car DBRefs with bruce as owner:
 > [Just (Car {owner = DBRef "Person bruce", cname = "Bat Mobile"})
 > ,Just (Car {owner = DBRef "Person bruce", cname = "Porsche"})]
 
-try to write with 'writeDBRef'
+try to write with 'writeDBRef':
 
 >>> atomically . writeDBRef bruce $ Person "Other"
 >*** Exception: writeDBRef: law of key conservation broken: old , new= Person bruce , Person Other
 
-DBRef's can not be written with objects of different keys
+DBRef's can not be written with objects of different keys:
 
 >>> atomically . writeDBRef bruce $ Person "Bruce"
 
 >>> let Just carReg1= head carRegs
 
-now from the Car register it is possible to recover the owner's register
+now from the Car register it is possible to recover the owner's register:
 
 >>> atomically $ readDBRef ( owner carReg1)
 >Just (Person {pname = "bruce"})
 
-
-
-DBRefs, once the pointed cached object is looked up in the cache and found at creation, they does
+DBRefs, once the referenced, cached object is looked up in the cache and found at creation, do
 not perform any further cache lookup afterwards, so reads and writes from/to DBRefs are faster
-than *Resource(s) calls, which perform lookups everytime
-in the cache
+than *Resource(s) calls, which perform cache lookups every time the object is accessed.
 
-DBRef's and *Resource(s) primitives are completely interoperable. The latter operate implicitly with DBRef's
+DBRefs and @*Resource(s)@ primitives are completely interoperable. The latter operate implicitly with DBRefs
 
 -}
 
@@ -135,16 +130,17 @@ DBRef's and *Resource(s) primitives are completely interoperable. The latter ope
 ,delDBRef
 
 -- * IResource class
-{- | cached objects must be instances of IResource.
-Such instances can be implicitly derived trough auxiliary clasess for file persistence
+{- | Cached objects must be instances of `IResource`.
+Such instances can be implicitly derived trough auxiliary classes for file persistence.
 -}
 ,IResource(..)
 
 -- * Operations with cached objects
-{- | Operations with DBRef's can be performed implicitly with the \"traditional\" TCache operations
-available in older versions.
+{- | Implement inversion of control primitives where the user defines the objects to retrieve. The primitives
+then call the defined function that determines how to transform the retrieved objects, which are sent
+back to the storage and a result is returned.
 
-In this example \"buy\" is a transaction where the user buy an item.
+In this example \"buy\" is a transaction where the user buys an item.
 The spent amount is increased and the stock of the product is decreased:
 
 @
@@ -152,11 +148,11 @@ data  Data=   User{uname:: String, uid:: String, spent:: Int} |
               Item{iname:: String, iid:: String, price:: Int, stock:: Int}
               deriving (Read, Show)
 
-instance Indexable Data where
-        key   User{uid=id}= id
-        key   Item{iid=id}= id
+instance `Indexable` Data where
+        `key`   User{uid=id}= id
+        `key`   Item{iid=id}= id
 
-user `buy` item=  'withResources'[user,item] buyIt
+user `buy` item= 'withResources'[user,item] buyIt
  where
     buyIt[Just us,Just it]
        | stock it > 0= [us',it']
@@ -272,7 +268,7 @@ import Debug.Trace
 -- there are two references to the DBRef here
 -- The Maybe one keeps it alive until the cache releases it for *Resources
 -- calls which does not reference dbrefs explicitly
--- The weak reference keeps the dbref alive until is it not referenced elsewere
+-- The weak reference keeps the dbref alive until is it not referenced elsewhere
 data CacheElem= forall a.(IResource a,Typeable a) => CacheElem (Maybe (DBRef a)) (Weak(DBRef a))
 
 type Ht = HashTable String  CacheElem
@@ -282,14 +278,14 @@ data ToWrite= forall a.(Typeable a,IResource a ) => ToWrite (DBRef a)
 instance Eq ToWrite where
    (ToWrite x)==(ToWrite y)= keyObjDBRef x == keyObjDBRef y
    
--- contains the hastable, last sync time and te list of references to be written in permanent storage
+-- contains the hashtable, last sync time and te list of references to be written in permanent storage
 type Cache = IORef (Ht , Integer,[ToWrite])
 data CheckTPVarFlags= AddToHash | NoAddToHash  
 
 
 data SyncMode= Synchronous   -- ^ write state after every step
-             | Asyncronous
-                  {frecuency  :: Int                     -- ^ number of seconds between saves when asyncronous
+             | Asynchronous
+                  {frequency  :: Int                     -- ^ number of seconds between saves when asynchronous
                   ,check      :: (Integer-> Integer-> Integer-> Bool)  -- ^ The user-defined check-for-cleanup-from-cache for each object. 'defaultCheck' is an example
                   ,cacheSize  :: Int                     -- ^ size of the cache when async
                   }
@@ -305,10 +301,10 @@ tvSyncWrite= unsafePerformIO $ newIORef  (Synchronous, Nothing)
 --
 -- For fast workflows, or when TCache` is used also for other purposes ,  `Asynchronous` is the best option
 --
--- `Asynchronous` mode  invokes `clearSyncCache`. For more complex use of the syncronization
+-- `Asynchronous` mode  invokes `clearSyncCache`. For more complex use of the synchronization
 -- please use this `clearSyncCache`.
 --
--- When interruptions are  controlled, use `SyncManual` mode and include a call to `syncCache` in the finalizaton code
+-- When interruptions are  controlled, use `SyncManual` mode and include a call to `syncCache` in the finalization code
 
 syncWrite::  (Monad m, MonadIO m) => SyncMode -> m ()
 syncWrite mode= do
@@ -317,7 +313,7 @@ syncWrite mode= do
      case mode of
           Synchronous -> modeWrite
           SyncManual  -> modeWrite
-          Asyncronous time check maxsize -> do
+          Asynchronous time check maxsize -> do
                th <- liftIO  $ clearSyncCacheProc  time check maxsize >> return()
                liftIO $ writeIORef tvSyncWrite (mode,Just th)
      where
@@ -388,9 +384,9 @@ newCache =do
         c <- H.new (==) hashString
         return (c,0,[])
 
--- | return the  total number of DBRefs in the cache. For debug purposes
--- This does not count the number of objects in the cache since many of the DBRef
--- may not have the pointed object loaded. Itś O(n).
+-- | Return the  total number of DBRefs in the cache. For debug purposes.
+-- This does not count the number of objects in the cache since many of the 'DBRef's
+-- may not have the referenced object loaded. It's O(n).
 numElems :: IO Int
 numElems= do
    (cache, _,_) <- readIORef refcache
@@ -464,16 +460,15 @@ instance Eq (DBRef a) where
 instance Ord (DBRef a) where
   compare (DBRef k _)  (DBRef k' _) = compare k k'
 
--- | return the key of the object pointed to by the DBRef
+-- | Return the key of the object referenced by the DBRef
 keyObjDBRef ::  DBRef a -> String
 keyObjDBRef (DBRef k _)= k
 
-
--- | get the reference to the object in the cache. if it does not exist, the reference is created empty.
+-- | Get the reference to the object in the cache. If it does not exist, the reference is created empty.
 -- Every execution of 'getDBRef' returns the same unique reference to this key,
--- so it can be safely considered pure. This is a property useful because deserialization
--- of objects with unused embedded DBRef's  do not need to marshall them eagerly
---  Tbis also avoid unnecesary cache lookups of the pointed objects.
+-- so it can be safely considered pure. This property is useful because deserialization
+-- of objects with unused embedded 'DBRef's do not need to marshall them eagerly.
+--  This also avoids unnecessary cache lookups of the referenced objects.
 {-# NOINLINE getDBRef #-}
 getDBRef :: (Typeable a, IResource a) => String -> DBRef a
 getDBRef key=   unsafePerformIO $! getDBRef1 $! key where
@@ -586,13 +581,8 @@ delDBRef dbref@(DBRef k tv)= do
 
    Nothing -> return ()
 
-    		
-
-
-
-
--- | deletes the pointed object from the cache, not the database (see 'delDBRef')
--- useful for cache invalidation when the database is modified by other process
+-- | Deletes the referenced object from the cache, not the database (see 'delDBRef')
+-- useful for cache invalidation when the database is modified by other processes.
 flushDBRef ::  (IResource a, Typeable a) =>DBRef a -> STM()
 flushDBRef (DBRef _ tv)=   writeTVar  tv  NotRead
 
